11459639480e2dba527c76d1ec84d2c015b3466c
Eric Lippmann <eric.lippmann@icinga.com>
2019-08-15 15:49:27 +0200

    Test Query::getDb() and ::setDb()

--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -29,4 +29,28 @@ class QueryTest extends \PHPUnit\Framework\TestCase
     {
         (new Query())->setModel('invalid');
     }
+
+    public function testGetDbReturnsNullIfUnset()
+    {
+        $query = new Query();
+
+        $this->assertNull($query->getDb());
+    }
+
+    public function testGetDbReturnsCorrectDbIfSet()
+    {
+        $db = new TestConnection();
+        $query = (new Query())
+            ->setDb($db);
+
+        $this->assertSame($db, $query->getDb());
+    }
+
+    /**
+     * @expectedException \TypeError
+     */
+    public function testSetDbThrowsExceptionOnTypeMismatch()
+    {
+        (new Query())->setDb('invalid');
+    }
 }
