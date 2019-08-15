2c65f8ca129ee62ab838ca4543852fde40c3e4e4
Eric Lippmann <eric.lippmann@icinga.com>
2019-08-15 15:21:27 +0200

    Add Query::getModel() and ::setModel()

--- src/Query.php
+++ src/Query.php
@@ -7,4 +7,30 @@ namespace ipl\Orm;
  */
 class Query
 {
+    /** @var Model Model to query */
+    protected $model;
+
+    /**
+     * Get the model to query
+     *
+     * @return Model
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * Set the model to query
+     *
+     * @param $model
+     *
+     * @return $this
+     */
+    public function setModel(Model $model)
+    {
+        $this->model = $model;
+
+        return $this;
+    }
 }
