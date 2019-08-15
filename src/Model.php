a71b8643fa6825e4c80a1ca803eb8eefbad49048
Eric Lippmann <eric.lippmann@icinga.com>
2019-08-15 14:37:37 +0200

    Introduce yet empty class Model

---
+++ src/Model.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ipl\Orm;
+
+/**
+ * Models represent single database tables or parts of it.
+ * They are also used to interact with the tables, i.e. in order to query for data.
+ */
+abstract class Model
+{
+}
