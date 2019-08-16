b969d403a58d09c6add24eabf0f27369cbd21e89
Eric Lippmann <eric.lippmann@icinga.com>
2019-08-16 09:14:30 +0200

    Add yet empty class Relation

---
+++ src/Relation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ipl\Orm;
+
+/**
+ * Relations represent the connection between models, i.e. the association between rows in one or more tables
+ * on the basis of matching key columns. The relationships are defined using candidate key-foreign key constructs.
+ */
+class Relation
+{
+}
