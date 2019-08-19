6df1ec6d719e95c66d849ca2791c9218474605a5
Eric Lippmann <eric.lippmann@icinga.com>
2019-08-19 15:30:54 +0200

    Add Relations::create()

--- src/Relations.php
+++ src/Relations.php
@@ -7,4 +7,27 @@ namespace ipl\Orm;
  */
 class Relations
 {
+    /** @var Relation[] */
+    protected $relations = [];
+
+    /**
+     * Create and add a new relation with the given name and target model class
+     *
+     * @param string $name        Name of the relation
+     * @param string $targetClass Target model class
+     *
+     * @return Relation
+     *
+     * @throws \InvalidArgumentException If the target model class is not of type string
+     */
+    public function create($name, $targetClass)
+    {
+        $relation = (new Relation())
+            ->setName($name)
+            ->setTargetClass($targetClass);
+
+        $this->relations[$name] = $relation;
+
+        return $relation;
+    }
 }
