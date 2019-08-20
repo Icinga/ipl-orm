95724e7fceb332987722902e796cce2284ab2dde
Eric Lippmann <eric.lippmann@icinga.com>
2019-08-20 02:38:40 +0200

    Test Relations::has()

--- tests/RelationsTest.php
+++ tests/RelationsTest.php
@@ -16,4 +16,19 @@ class RelationsTest extends \PHPUnit\Framework\TestCase
         $this->assertSame($name, $relation->getName());
         $this->assertSame($targetClass, $relation->getTargetClass());
     }
+
+    public function testHasReturnsTrueIfRelationExists()
+    {
+        $relations = new Relations();
+        $relations->create('test', TestModel::class);
+
+        $this->assertTrue($relations->has('test'));
+    }
+
+    public function testHasReturnsFalseIfRelationDoesNotExist()
+    {
+        $relations = new Relations();
+
+        $this->assertFalse($relations->has('test'));
+    }
 }
