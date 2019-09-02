537df9637fee6b7e060837c782eff8f543e7f0da
Eric Lippmann <eric.lippmann@icinga.com>
2019-09-02 10:47:43 +0200

    Add Relations::get()

--- src/Relations.php
+++ src/Relations.php
@@ -23,6 +23,22 @@ class Relations
     }
 
     /**
+     * Get the relation with the given name
+     *
+     * @param string $name
+     *
+     * @return Relation
+     *
+     * @throws \InvalidArgumentException If the relation with the given name does not exist
+     */
+    public function get($name)
+    {
+        $this->assertRelationExists($name);
+
+        return $this->relations[$name];
+    }
+
+    /**
      * Create and add a new relation with the given name and target model class
      *
      * @param string $name        Name of the relation
@@ -55,6 +71,18 @@ class Relations
     {
         if ($this->has($name)) {
             throw new \InvalidArgumentException("Relation '$name' already exists");
+        }
+    }
+
+    /**
+     * Throw exception if a relation with the given name does not exist
+     *
+     * @param string $name
+     */
+    protected function assertRelationExists($name)
+    {
+        if (! $this->has($name)) {
+            throw new \InvalidArgumentException("Can't access relation '$name'. Relation not found");
         }
     }
 }
