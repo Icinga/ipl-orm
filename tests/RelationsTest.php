d6ffacc399442ec2f79e80b7fa5f94ccbfa8612f
Eric Lippmann <eric.lippmann@icinga.com>
2019-09-03 15:40:38 +0200

    Test relationship definition functions

--- tests/RelationsTest.php
+++ tests/RelationsTest.php
@@ -2,6 +2,10 @@
 
 namespace ipl\Tests\Orm;
 
+use ipl\Orm\Relation\BelongsTo;
+use ipl\Orm\Relation\BelongsToMany;
+use ipl\Orm\Relation\HasMany;
+use ipl\Orm\Relation\HasOne;
 use ipl\Orm\Relations;
 
 class RelationsTest extends \PHPUnit\Framework\TestCase
@@ -62,5 +66,37 @@ class RelationsTest extends \PHPUnit\Framework\TestCase
     public function testGetThrowsInvalidArgumentExceptionIfRelationDoesNotExist()
     {
         (new Relations())->get('test');
+    }
+
+    public function testHasOneReturnsOneToOneRelationship()
+    {
+        $relation = (new Relations())->hasOne('test', TestModel::class);
+
+        /** @noinspection PhpParamsInspection */
+        $this->assertInstanceOf(HasOne::class, $relation);
+    }
+
+    public function testHasManyReturnsOneToManyRelationship()
+    {
+        $relation = (new Relations())->hasMany('test', TestModel::class);
+
+        /** @noinspection PhpParamsInspection */
+        $this->assertInstanceOf(HasMany::class, $relation);
+    }
+
+    public function testBelongsToReturnsTheInverseRelationship()
+    {
+        $relation = (new Relations())->belongsTo('test', TestModel::class);
+
+        /** @noinspection PhpParamsInspection */
+        $this->assertInstanceOf(BelongsTo::class, $relation);
+    }
+
+    public function testBelongsToManyReturnsManyToManyRelationship()
+    {
+        $relation = (new Relations())->belongsToMany('test', TestModel::class);
+
+        /** @noinspection PhpParamsInspection */
+        $this->assertInstanceOf(BelongsToMany::class, $relation);
     }
 }
