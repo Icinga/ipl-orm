9dbdaa64789aa1b3d40b623df8c2b004ee0135de
Eric Lippmann <eric.lippmann@icinga.com>
2019-09-26 11:49:37 +0200

    Support custom junction model classes in BelogsToMany::setThrough()

--- src/Relation/BelongsToMany.php
+++ src/Relation/BelongsToMany.php
@@ -4,13 +4,15 @@ namespace ipl\Orm\Relation;
 
 use ipl\Orm\Model;
 use ipl\Orm\Relation;
+use ipl\Orm\Relations;
+use function ipl\Stdlib\get_php_type;
 
 /**
  * Many-to-many relationship
  */
 class BelongsToMany extends Relation
 {
-    /** @var string Name of the join table */
+    /** @var string Name of the join table or junction model class */
     protected $through;
 
     /** @var string|array Column name(s) of the target model's foreign key found in the join table */
@@ -20,7 +22,7 @@ class BelongsToMany extends Relation
     protected $targetCandidateKey;
 
     /**
-     * Get the name of the join table
+     * Get the name of the join table or junction model class
      *
      * @return string
      */
@@ -30,7 +32,7 @@ class BelongsToMany extends Relation
     }
 
     /**
-     * Set the name of the join table
+     * Set the join table name or junction model class
      *
      * @param string $through
      *
@@ -93,26 +95,69 @@ class BelongsToMany extends Relation
 
     public function resolve(Model $source)
     {
-        $through = $this->getThrough();
-
-        $junction = (new Junction())
-            ->setTableName($through);
-
-        $toJunction = (new HasMany())
-            ->setName($through)
-            ->setTarget($junction)
-            ->setCandidateKey($this->getCandidateKey())
-            ->setForeignKey($this->getForeignKey());
+        $possibleCandidateKey = [$this->getCandidateKey()];
+        $possibleForeignKey = [$this->getForeignKey()];
 
         $target = $this->getTarget();
 
+        $possibleTargetCandidateKey = [$this->getTargetForeignKey() ?: static::getDefaultForeignKey($target)];
+        $possibleTargetForeignKey = [$this->getTargetCandidateKey() ?: static::getDefaultCandidateKey($target)];
+
+        $through = $this->getThrough();
+
+        if (class_exists($through)) {
+            $junction = new $through();
+
+            if (! $junction instanceof Model) {
+                throw new \InvalidArgumentException(sprintf(
+                    'Junction model class must be an instance of %s, %s given',
+                    Model::class,
+                    get_php_type($junction)
+                ));
+            }
+
+            $relations = new Relations();
+            $junction->createRelations($relations);
+
+            if ($relations->has($source->getTableName())) {
+                $sourceRelation = $relations->get($source->getTableName());
+
+                $possibleCandidateKey[] = $sourceRelation->getForeignKey();
+                $possibleForeignKey[] = $sourceRelation->getCandidateKey();
+            }
+
+            if ($relations->has($target->getTableName())) {
+                $targetRelation = $relations->get($target->getTableName());
+
+                $possibleTargetCandidateKey[] = $targetRelation->getForeignKey();
+                $possibleTargetForeignKey[] = $targetRelation->getCandidateKey();
+            }
+        } else {
+            $junction = (new Junction())
+                ->setTableName($through);
+        }
+
+        $toJunction = (new HasMany())
+            ->setName($junction->getTableName())
+            ->setTarget($junction)
+            ->setCandidateKey($this->extractKey($possibleCandidateKey))
+            ->setForeignKey($this->extractKey($possibleForeignKey));
+
         $toTarget = (new HasMany())
             ->setName($this->getName())
-            ->setTarget($this->getTarget())
-            ->setCandidateKey($this->getTargetForeignKey() ?: static::getDefaultForeignKey($target))
-            ->setForeignKey($this->getTargetCandidateKey() ?: static::getDefaultCandidateKey($target));
+            ->setTarget($target)
+            ->setCandidateKey($this->extractKey($possibleTargetCandidateKey))
+            ->setForeignKey($this->extractKey($possibleTargetForeignKey));
+
 
         yield from $toJunction->resolve($source);
         yield from $toTarget->resolve($junction);
+    }
+
+    protected function extractKey(array $possibleKey)
+    {
+        $filtered = array_filter($possibleKey);
+
+        return array_pop($filtered);
     }
 }
