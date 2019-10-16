b5cb2029a6a8d6ba6e9f8202b95f5c49ebe3e168
Eric Lippmann <eric.lippmann@icinga.com>
2019-10-16 09:20:21 +0200

    Introduce compat class FilterProcessor

---
+++ src/Compat/FilterProcessor.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace ipl\Orm\Compat;
+
+use Icinga\Data\Filter\Filter;
+use Icinga\Data\Filter\FilterAnd;
+use Icinga\Data\Filter\FilterChain;
+use Icinga\Data\Filter\FilterExpression;
+use Icinga\Data\Filter\FilterNot;
+use Icinga\Data\Filter\FilterOr;
+use ipl\Orm\Query;
+use ipl\Sql\Expression;
+use ipl\Sql\Sql;
+
+class FilterProcessor
+{
+    public static function assembleFilter(Filter $filter, $level = 0)
+    {
+        $condition = null;
+
+        if ($filter->isChain()) {
+            if ($filter instanceof FilterAnd) {
+                $operator = Sql::ALL;
+            } elseif ($filter instanceof FilterOr) {
+                $operator = Sql::ANY;
+            } elseif ($filter instanceof FilterNot) {
+                $operator = 'NOT'; // TODO(el): Sql::NOT does not exist yet
+            } else {
+                throw new \InvalidArgumentException(sprintf('Cannot render filter: %s', get_class($filter)));
+            }
+
+            if (! $filter->isEmpty()) {
+                foreach ($filter->filters() as $filterPart) {
+                    $part = static::assembleFilter($filterPart, $level + 1);
+                    if ($part) {
+                        if ($condition === null) {
+                            $condition = [$operator, $part];
+                        } else {
+                            if ($condition[0] === $operator) {
+                                $condition[] = $part;
+                            } else {
+                                $condition = [$operator, $condition, $part];
+                            }
+                        }
+                    }
+                }
+            } else {
+                // TODO(el): Explicitly return the empty string due to the FilterNot case?
+            }
+        } else {
+            /** @var FilterExpression $filter */
+            $condition = array_merge(
+                [Sql::ALL],
+                static::assemblePredicate($filter->getColumn(), $filter->getSign(), $filter->getExpression())
+            );
+        }
+
+        return $condition;
+    }
+
+    public static function assemblePredicate($column, $operator, $expression)
+    {
+        if (is_array($expression)) {
+            if ($operator === '=') {
+                return ["$column IN (?)" => $expression];
+            } elseif ($operator === '!=') {
+                return ["($column NOT IN (?) OR $column IS NULL)" => $expression];
+            }
+
+            throw new \InvalidArgumentException(
+                'Unable to render array expressions with operators other than equal or not equal'
+            );
+        } elseif ($operator === '=' && strpos($expression, '*') !== false) {
+            if ($expression === '*') {
+                // We'll ignore such filters as it prevents index usage and because "*" means anything. So whether we're
+                // using a real column with a valid comparison here or just an expression which can only be evaluated to
+                // true makes no difference, except for performance reasons
+                return [new Expression('TRUE')];
+            }
+
+            return ["$column LIKE ?" => str_replace('*', '%', $expression)];
+        } elseif ($operator === '!=' && strpos($expression, '*') !== false) {
+            if ($expression === '*') {
+                // We'll ignore such filters as it prevents index usage and because "*" means nothing. So whether we're
+                // using a real column with a valid comparison here or just an expression which cannot be evaluated to
+                // true makes no difference, except for performance reasons
+                return [new Expression('FALSE')];
+            }
+
+            return ["($column NOT LIKE ? OR $column IS NULL)" => str_replace('*', '%', $expression)];
+        } elseif ($operator === '!=') {
+            return ["($column != ? OR $column IS NULL)" => $expression];
+        } else {
+            return ["$column $operator ?" => $expression];
+        }
+    }
+
+    public static function apply(Filter $filter, Query $query)
+    {
+        if (! $filter->isEmpty()) {
+            $filter = clone $filter;
+
+            static::requireAndResolveFilterColumns($filter, $query);
+
+            $where = static::assembleFilter($filter);
+
+            if ($where) {
+                $operator = array_shift($where);
+
+                $query->getSelectBase()->where($where, $operator);
+            }
+        }
+    }
+
+    protected static function requireAndResolveFilterColumns(Filter $filter, Query $query)
+    {
+        if ($filter instanceof FilterExpression) {
+            if ($filter->getExpression() === '*') {
+                // Wildcard only filters are ignored so stop early here to avoid joining a table for nothing
+                return;
+            }
+
+            $column = $filter->getColumn();
+
+            $dot = strrpos($column, '.');
+
+            if ($dot !== false) {
+                $relation = substr($column, 0, $dot);
+                $column = substr($column, $dot + 1);
+
+                if ($relation !== $query->getModel()->getTableName()) {
+                    $tableName = $query
+                        ->with($relation)
+                        ->getWith()[$relation]
+                        ->getTarget()
+                        ->getTableName();
+
+                    $filter->setColumn($tableName . '.' . $column);
+                }
+            }
+        } else {
+            /** @var FilterChain $filter */
+            foreach ($filter->filters() as $child) {
+                static::requireAndResolveFilterColumns($child, $query);
+            }
+        }
+    }
+}
