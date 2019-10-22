a6f5d2c6c5eb4721aaad0c3dbf1b8d56b745c07e
Johannes Meyer <johannes.meyer@icinga.com>
2019-10-22 13:16:32 +0200

    Behaviors: Differentiate between retrieval and persistence
    Expects behaviors of type `Behavior` now. Though, `Behavior` is
    just a internal interface. Implementors are expected to use
    the interfaces or abstracts in namespace `ipl\Orm\Contract`.
    These are also the ones which define how the differentation
    should be handled.

--- src/Behaviors.php
+++ src/Behaviors.php
@@ -2,32 +2,98 @@
 
 namespace ipl\Orm;
 
-use ipl\Orm\Contract\BehaviorInterface;
+use ipl\Orm\Contract\PersistBehavior;
+use ipl\Orm\Contract\PropertyBehavior;
+use ipl\Orm\Contract\RetrieveBehavior;
 
 class Behaviors
 {
-    /** @var BehaviorInterface[] Registered behaviors */
-    protected $behaviors = [];
+    /** @var RetrieveBehavior[] Registered retrieve behaviors */
+    protected $retrieveBehaviors = [];
+
+    /** @var PersistBehavior[] Registered persist behaviors */
+    protected $persistBehaviors = [];
+
+    /** @var PropertyBehavior[] Registered property behaviors */
+    protected $propertyBehaviors = [];
 
     /**
      * Add a behavior
      *
-     * @param BehaviorInterface $behavior
+     * @param PersistBehavior|PropertyBehavior|RetrieveBehavior $behavior
      */
-    public function add($behavior)
+    public function add(Behavior $behavior)
     {
-        $this->behaviors[] = $behavior;
+        if ($behavior instanceof PropertyBehavior) {
+            $this->retrieveBehaviors[] = $behavior;
+            $this->persistBehaviors[] = $behavior;
+            $this->propertyBehaviors[] = $behavior;
+        } else {
+            if ($behavior instanceof RetrieveBehavior) {
+                $this->retrieveBehaviors[] = $behavior;
+            }
+
+            if ($behavior instanceof PersistBehavior) {
+                $this->persistBehaviors[] = $behavior;
+            }
+        }
     }
 
     /**
-     * Apply all behaviors on the given model
+     * Apply all retrieve behaviors on the given model
      *
      * @param Model $model
      */
-    public function apply(Model $model)
+    public function retrieve(Model $model)
     {
-        foreach ($this->behaviors as $behavior) {
-            $behavior->apply($model);
+        foreach ($this->retrieveBehaviors as $behavior) {
+            $behavior->retrieve($model);
         }
+    }
+
+    /**
+     * Apply all persist behaviors on the given model
+     *
+     * @param Model $model
+     */
+    public function persist(Model $model)
+    {
+        foreach ($this->persistBehaviors as $behavior) {
+            $behavior->persist($model);
+        }
+    }
+
+    /**
+     * Transform the retrieved key's value by use of all property behaviors
+     *
+     * @param mixed  $value
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function retrieveProperty($value, $key)
+    {
+        foreach ($this->propertyBehaviors as $behavior) {
+            $value = $behavior->retrieveProperty($value, $key);
+        }
+
+        return $value;
+    }
+
+    /**
+     * Transform the to be persisted key's value by use of all property behaviors
+     *
+     * @param mixed  $value
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function persistProperty($value, $key)
+    {
+        foreach ($this->propertyBehaviors as $behavior) {
+            $value = $behavior->persistProperty($value, $key);
+        }
+
+        return $value;
     }
 }
