c8696cd10b31805a4eb3445ce235c6ba990bab97
Johannes Meyer <johannes.meyer@icinga.com>
2019-10-23 08:09:12 +0200

    FilterProcessor: Utilize persist behaviors of targeted models

--- src/Compat/FilterProcessor.php
+++ src/Compat/FilterProcessor.php
@@ -29,7 +29,8 @@ class FilterProcessor extends \ipl\Sql\Compat\FilterProcessor
     protected static function requireAndResolveFilterColumns(Filter $filter, Query $query)
     {
         if ($filter instanceof FilterExpression) {
-            if ($filter->getExpression() === '*') {
+            $expression = $filter->getExpression();
+            if ($expression === '*') {
                 // Wildcard only filters are ignored so stop early here to avoid joining a table for nothing
                 return;
             }
@@ -37,24 +38,27 @@ class FilterProcessor extends \ipl\Sql\Compat\FilterProcessor
             $column = $filter->getColumn();
 
             $dot = strrpos($column, '.');
-
             if ($dot !== false) {
-                $relation = substr($column, 0, $dot);
+                $relationName = substr($column, 0, $dot);
                 $column = substr($column, $dot + 1);
 
-                if ($relation !== $query->getModel()->getTableName()) {
-                    $tableName = $query
-                        ->with($relation)
-                        ->getWith()[$relation]
-                        ->getTableAlias();
-
-                    $filter->setColumn($tableName . '.' . $column);
+                if ($relationName !== $query->getModel()->getTableName()) {
+                    $relation = $query->with($relationName)
+                        ->getWith()[$relationName];
+                    $expression = $query->getBehaviors($relation->getTarget())
+                        ->persistProperty($expression, $column);
+                    $column = $relation->getTableAlias() . '.' . $column;
                 } else {
-                    $filter->setColumn( $query->getModel()->getTableName() . '.' . $column);
+                    $expression = $query->getBehaviors()->persistProperty($expression, $column);
+                    $column = $query->getModel()->getTableName() . '.' . $column;
                 }
             } else {
-                $filter->setColumn( $query->getModel()->getTableName() . '.' . $column);
+                $expression = $query->getBehaviors()->persistProperty($expression, $column);
+                $column = $query->getModel()->getTableName() . '.' . $column;
             }
+
+            $filter->setColumn($column);
+            $filter->setExpression($expression);
         } else {
             /** @var FilterChain $filter */
             foreach ($filter->filters() as $child) {
