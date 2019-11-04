486f48c8142ac8bb2b92320120759ce7d97440b5
Johannes Meyer <johannes.meyer@icinga.com>
2019-11-04 15:04:46 +0100

    FilterProcessor: Transform filters causing to-many joins to sub-queries

--- src/Compat/FilterProcessor.php
+++ src/Compat/FilterProcessor.php
@@ -3,13 +3,17 @@
 namespace ipl\Orm\Compat;
 
 use Icinga\Data\Filter\Filter;
+use Icinga\Data\Filter\FilterAnd;
 use Icinga\Data\Filter\FilterChain;
 use Icinga\Data\Filter\FilterExpression;
+use Icinga\Data\Filter\FilterOr;
 use ipl\Orm\Query;
 use ipl\Orm\UnionQuery;
 
 class FilterProcessor extends \ipl\Sql\Compat\FilterProcessor
 {
+    protected $madeJoins = [];
+
     public static function apply(Filter $filter, Query $query)
     {
         if ($query instanceof UnionQuery) {
@@ -23,7 +27,7 @@ class FilterProcessor extends \ipl\Sql\Compat\FilterProcessor
         if (! $filter->isEmpty()) {
             $filter = clone $filter;
 
-            $rewrittenFilter = static::requireAndResolveFilterColumns($filter, $query);
+            $rewrittenFilter = (new static())->requireAndResolveFilterColumns($filter, $query);
             if ($rewrittenFilter !== null) {
                 $filter = $rewrittenFilter;
             }
@@ -39,7 +43,7 @@ class FilterProcessor extends \ipl\Sql\Compat\FilterProcessor
         }
     }
 
-    protected static function requireAndResolveFilterColumns(Filter $filter, Query $query)
+    protected function requireAndResolveFilterColumns(Filter $filter, Query $query)
     {
         if ($filter instanceof FilterExpression) {
             $expression = $filter->getExpression();
@@ -50,19 +54,20 @@ class FilterProcessor extends \ipl\Sql\Compat\FilterProcessor
 
             $baseTable = $query->getModel()->getTableName();
             $column = $filter->getColumn();
-
-            $dot = strrpos($column, '.');
-            if ($dot !== false) {
-                $relations = explode('.', substr($column, 0, $dot));
-                if ($relations[0] !== $baseTable) {
-                    // Prepend the base table if missing to ensure we'll deal only with absolute paths next
-                    array_unshift($relations, $baseTable);
-                    $column = $baseTable . '.' . $column;
-                }
-            } else {
-                $relations = [$baseTable];
-                $column = $baseTable . '.' . $column;
+            if (isset($filter->metaData)) {
+                $column = $filter->metaData['relationCol'];
             }
+
+            $column = $query->getResolver()->qualifyPath($column, $baseTable);
+            $filter->metaData['column'] = $column;
+
+            // TODO: legacy filter columns? (with underscores)
+            list($relationPath, $columnName) = preg_split('/\.(?=[^.]+$)/', $column);
+
+            $filter->metaData['relationPath'] = $relationPath;
+            $filter->metaData['relationCol'] = $columnName;
+
+            $relations = explode('.', $relationPath);
 
             do {
                 $relationName = array_shift($relations);
@@ -81,25 +86,124 @@ class FilterProcessor extends \ipl\Sql\Compat\FilterProcessor
                 $rewrittenFilter = $query->getBehaviors($subject)
                     ->rewriteCondition((clone $filter)->setColumn($columnName), $path . '.');
                 if ($rewrittenFilter !== null) {
-                    static::requireAndResolveFilterColumns($rewrittenFilter, $query);
+                    if (isset($rewrittenFilter->transferMetaData) || $rewrittenFilter instanceof $filter) {
+                        // The filter hasn't changed semantically
+                        $rewrittenFilter->metaData['relationPath'] = $path;
+                        $rewrittenFilter->metaData['relationCol'] = $columnName;
+                        $rewrittenFilter->metaData['original'] = $filter;
+                    }
+
+                    if (isset($relation)) {
+                        $this->madeJoins[$path][] = $rewrittenFilter;
+                    }
+
+                    $this->requireAndResolveFilterColumns($rewrittenFilter, $query);
                     return $rewrittenFilter;
+                } elseif (isset($relation)) {
+                    $this->madeJoins[$path][] = $filter;
                 }
             } while (! empty($relations));
 
             $expression = $query->getBehaviors($subject)->persistProperty($expression, $columnName);
-            if (isset($relation)) {
-                $column = $query->getResolver()->getAlias($relation->getTarget()) . '.' . $columnName;
-            }
+            $column = $query->getResolver()->qualifyPath($columnName, $query->getResolver()->getAlias(
+                isset($relation) ? $relation->getTarget() : $query->getModel()
+            ));
 
             $filter->setColumn($column);
             $filter->setExpression($expression);
         } else {
             /** @var FilterChain $filter */
+
+            $subQueryFilters = [];
             foreach ($filter->filters() as $child) {
                 /** @var Filter $child */
-                $rewrittenFilter = static::requireAndResolveFilterColumns($child, $query);
+                $rewrittenFilter = $this->requireAndResolveFilterColumns($child, $query);
                 if ($rewrittenFilter !== null) {
                     $filter->replaceById($child->getId(), $rewrittenFilter);
+                    $child = $rewrittenFilter;
+                }
+
+                // We optimize only single expressions or chains with explicit meta-data
+                if (! isset($child->noOptmization) && ($child instanceof FilterExpression || isset($child->metaData))) {
+                    $child->noOptmization = true;
+
+                    $relationPath = $child->metaData['relationPath'];
+                    if ($relationPath !== $query->getModel()->getTableName()) {
+                        if (! $query->getWith()[$relationPath]->isOne()) {
+                            if (isset($child->metaData['original'])) {
+                                $column = $child->metaData['original']->metaData['column'];
+                                $sign = $child->metaData['original']->getSign();
+                            } else {
+                                $column = $child->getColumn();
+                                $sign = $child->getSign();
+                            }
+
+                            $subQueryFilters[$sign][$column][] = $child;
+                        }
+                    }
+                }
+            }
+
+            foreach ($subQueryFilters as $sign => $filterCombinations) {
+                foreach ($filterCombinations as $column => $filters) {
+                    // The relation path must be the same for all entries
+                    $subQuery = $query->createSubQuery($filters[0]->metaData['relationPath']);
+
+                    if ($sign === '!=' || $filter instanceof FilterAnd) {
+                        $targetKeys = join(',', array_values(
+                            $subQuery->getResolver()->qualifyColumns(
+                                (array) $subQuery->getModel()->getKeyName(),
+                                $subQuery->getResolver()->getAlias($subQuery->getModel())
+                            )
+                        ));
+
+                        if ($sign !== '!=' || $filter instanceof FilterOr) {
+                            // Unequal (!=) comparisons chained with an OR are considered an XOR
+                            $count = count($filters);
+                        } else {
+                            // Unequal (!=) comparisons are transformed to equal (=) ones. If chained with an AND
+                            // we just have to check for a single result as an object must not match any of these
+                            // comparisons
+                            $count = 1;
+                        }
+
+                        $subQuery->getSelectBase()->having(["COUNT(DISTINCT $targetKeys) >= ?" => $count]);
+                    }
+
+                    foreach ($filters as $i => $child) {
+                        $filter->removeId($child->getId());
+
+                        if ($sign === '!=') {
+                            // Unequal comparisons must be negated since the sub-query is an inverse of the outer one
+                            if ($child->isExpression()) {
+                                $filters[$i] = $child->setSign('=');
+                                $filters[$i]->metaData = $child->metaData;
+                            } else {
+                                $filters[$i] = Filter::not($child);
+                            }
+                        }
+
+                        // Remove joins solely used for filter conditions
+                        foreach ($this->madeJoins as $joinPath => $madeBy) {
+                            $madeBy = array_filter($madeBy, function ($relationFilter) use ($child) {
+                                return $child->getId() !== $relationFilter->getId()
+                                    && ! $child->hasId($relationFilter->getId());
+                            });
+
+                            if (empty($madeBy)) {
+                                $query->without($joinPath);
+                                unset($this->madeJoins[$joinPath]);
+                            }
+                        }
+                    }
+
+                    static::apply(Filter::matchAny($filters), $subQuery);
+
+                    $filter->addFilter(new FilterExpression(
+                        '',
+                        ($sign === '!=' ? 'NOT ' : '') . 'EXISTS',
+                        $subQuery->assembleSelect()
+                    ));
                 }
             }
         }
