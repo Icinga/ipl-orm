281cdefa9d43706a69c22368c3fc722c78f952f6
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2021-09-24 15:47:47 +0200

    ResultSetTest: Add tests for `cacheDisabled()`

--- tests/ResultSetTest.php
+++ tests/ResultSetTest.php
@@ -23,4 +23,43 @@ class ResultSetTest extends TestCase
             'ArrayIterator::offsetSet() (used in ResultSet::advance()) does not seek automatically on PHP 5.6'
         );
     }
+
+    public function testResultWithCacheDisabled()
+    {
+        $set = (new ResultSet(new ArrayIterator(['a', 'b', 'c'])))->disableCache();
+
+        $items = [];
+        foreach ($set as $item) {
+            $items[] = $item;
+        }
+
+        // When cache disabled, $set can be iterated only once, so this loop will be skipped
+        foreach ($set as $item) {
+            $items[] = $item;
+        }
+
+        $this->assertEquals(
+            $items,
+            ['a', 'b', 'c']
+        );
+    }
+
+    public function testResultWithCacheEnabled()
+    {
+        $set = (new ResultSet(new ArrayIterator(['a', 'b', 'c'])));
+
+        $items = [];
+        foreach ($set as $item) {
+            $items[] = $item;
+        }
+
+        foreach ($set as $item) {
+            $items[] = $item;
+        }
+
+        $this->assertEquals(
+            $items,
+            ['a', 'b', 'c', 'a', 'b', 'c']
+        );
+    }
 }
