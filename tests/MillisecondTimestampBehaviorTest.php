d424fe33c281ca1b5e170697b462379a5260ac35
Yonas Habteab <yonas.habteab@icinga.com>
2023-02-01 11:32:54 +0100

    Introduce `MillisecondTimestamp` behavior (#83)
    * Introduce `MillisecondTimestamp` behavior
    
    * Add `MillisecondTimestamp` behavior test cases
    
    * tests: Improve mil second behavior tests
    
    * tests: Add test case for invalid datetime
    
    * tests: Use `assertSame()` where applicable
    
    ---------
    
    Co-authored-by: Johannes Meyer <johannes.meyer@icinga.com>

---
+++ tests/MillisecondTimestampBehaviorTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace ipl\Tests\Orm;
+
+use DateTime;
+use DateTimeZone;
+use ipl\Orm\Behavior\MillisecondTimestamp;
+use ipl\Orm\Exception\ValueConversionException;
+use PHPUnit\Framework\TestCase;
+
+class MillisecondTimestampBehaviorTest extends TestCase
+{
+    public function testFromDbReturnsNullWhenNullIsPassed()
+    {
+        $this->assertNull((new MillisecondTimestamp([]))->fromDb(null, 'key', null));
+    }
+
+    public function testToDbReturnsUtcTimestampWithNonUtcInput()
+    {
+        $sometime = DateTime::createFromFormat(
+            'Y-m-d H:i:s',
+            '2022-11-09 15:00:00',
+            new DateTimeZone('Europe/Berlin')
+        );
+
+        $this->assertSame(1668002400000, (new MillisecondTimestamp([]))->toDb($sometime, 'key', null));
+    }
+
+    public function testToDbReturnsUtcTimestampWithUtcInput()
+    {
+        $sometime = DateTime::createFromFormat(
+            'Y-m-d H:i:s',
+            '2022-11-09 14:00:00',
+            new DateTimeZone('UTC')
+        );
+
+        $this->assertSame(1668002400000, (new MillisecondTimestamp([]))->toDb($sometime, 'key', null));
+
+        $sometime = 1668002400;
+
+        $this->assertSame(1668002400000, (new MillisecondTimestamp([]))->toDb($sometime, 'key', null));
+    }
+
+    /**
+     * @depends testToDbReturnsUtcTimestampWithNonUtcInput
+     */
+    public function testFromDbReturnsTimezoneAwareDateTime()
+    {
+        date_default_timezone_set('Europe/Berlin');
+
+        $this->assertEquals('2022-11-09 15:00:00', (new MillisecondTimestamp([]))
+            ->fromDb(1668002400000, 'key', null)->format('Y-m-d H:i:s'));
+    }
+
+    public function testFromDbReturnsUtcDateTime()
+    {
+        date_default_timezone_set('UTC');
+
+        $this->assertEquals('2022-11-09 14:00:00', (new MillisecondTimestamp([]))
+            ->fromDb(1668002400000, 'key', null)->format('Y-m-d H:i:s'));
+    }
+
+    public function testToDbPreservesMilliseconds()
+    {
+        $sometime = DateTime::createFromFormat(
+            'Y-m-d H:i:s.u',
+            '2022-11-09 14:00:00.123',
+            new DateTimeZone('UTC')
+        );
+
+        $this->assertSame(1668002400123, (new MillisecondTimestamp([]))->toDb($sometime, 'key', null));
+
+        $sometime = 1668002400.123;
+
+        $this->assertSame(1668002400123, (new MillisecondTimestamp([]))->toDb($sometime, 'key', null));
+
+        $sometime = '2022-11-09 14:00:00.123';
+
+        $this->assertSame(1668002400123, (new MillisecondTimestamp([]))->toDb($sometime, 'key', null));
+    }
+
+    public function testToDbThrowsExceptionWhenInvalidDatetimeIsPassed()
+    {
+        $this->expectException(ValueConversionException::class);
+
+        $this->assertSame(true, (new MillisecondTimestamp([]))->toDb('*', 'key', null));
+    }
+}
