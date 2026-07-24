8c4527c9b6d2d0e3b5ffdd59124dfef170a6fedc
Johannes Meyer <johannes.meyer@icinga.com>
2026-07-24 14:32:35 +0200

    Resolver: Resolve its filters when resolving a relation

--- src/Resolver.php
+++ src/Resolver.php
@@ -717,10 +717,13 @@ class Resolver
 
                 $relation = $targetRelations->get($relationName);
                 $relation->setSource($target);
+                $this->resolveRelationFilter($relation->getFilter(), $target, $relation->getTarget());
 
                 $resolvedRelations[$relationPath] = $relation;
 
                 if ($relation instanceof BelongsToMany) {
+                    $this->resolveRelationFilter($relation->getThroughFilter(), $target, $relation->getThrough());
+
                     $this->setAlias($relation->getThrough(), join('_', array_merge(
                         array_slice($segments, 0, -1),
                         [$relation->getThroughAlias()]
